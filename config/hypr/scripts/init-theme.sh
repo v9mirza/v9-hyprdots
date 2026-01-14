9931669d07e2e3b5d83586ed6eda07e915da21fa
v9mirza <v9mirza@gmail.com>
2026-01-14 20:23:17 +0000

    commit

--- config/hypr/scripts/init-theme.sh
+++ config/hypr/scripts/init-theme.sh
@@ -16,8 +16,13 @@ gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
 gsettings set org.gnome.desktop.interface cursor-size 24
 
-# Restart Tray Applets to pick up new theme
+# Restart Tray Applets (Force Environment)
 killall nm-applet blueman-applet udiskie 2>/dev/null || true
+sleep 0.5
+
+# Launch with explicit env to fix icons
+export GTK_THEME=Adwaita-dark
+export GTK_ICON_THEME=Papirus-Dark
 nm-applet --indicator &
 blueman-applet &
 udiskie &
